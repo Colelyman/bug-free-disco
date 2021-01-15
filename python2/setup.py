1a03c9f0154a952af12ca3781927a0a7bfa070ab
Cole Lyman <cole@colelyman.com>
2021-01-15 11:58:58 -0500

    Update setup.py to use language_level and updated include paths

--- python2/setup.py
+++ python2/setup.py
@@ -3,6 +3,10 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules=cythonize("*.pyx"),
-    include_dirs=[numpy.get_include()]
+    ext_modules=cythonize(
+        'python2/CRISPResso2Align.pyx',
+        build_dir='python2',
+        language_level=2,
+    ),
+    include_dirs=[numpy.get_include()],
 )
--- python3/setup.py
+++ python3/setup.py
@@ -3,6 +3,10 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    ext_modules=cythonize("*.pyx"),
-    include_dirs=[numpy.get_include()]
+    ext_modules=cythonize(
+        'python3/CRISPResso2Align.pyx',
+        build_dir='python3',
+        language_level=3,
+    ),
+    include_dirs=[numpy.get_include()],
 )
